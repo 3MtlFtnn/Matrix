a2efc6897b016cf6e68b2975455abeae31389887
Verkartn <verkartn@student.21-school.ru>
2024-04-07 16:19:25 +0300

    Структура проекта

---
+++ src/s21_matrix.h
@@ -0,0 +1,34 @@
+#ifndef S21_MATRIX_H
+#define S21_MATRIX_H
+
+#define SUCCESS 1
+#define FAILURE 0
+
+typedef struct matrix_struct {
+  double **matrix;
+  int rows;
+  int columns;
+} matrix_t;
+
+// Создание и удаление
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+
+// Сравнение
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+
+// Арифметические операции
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+/* умножение скалярного числа (number) */
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+/* умножение матриц */
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+
+// Другие операции
+int s21_transpose(matrix_t *A, matrix_t *result);
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_determinant(matrix_t *A, double *result);
+int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+
+#endif
