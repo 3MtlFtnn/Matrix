fee7b0b03f242575ecc41fd45d209d23c2a4300a
Blisspen <blisspen@student.21-school.ru>
2024-04-08 21:09:57 +0300

    poco

--- src/arithmetic/determinator_matrix.c
+++ src/arithmetic/determinator_matrix.c
@@ -16,7 +16,15 @@
 int s21_determinant(matrix_t *A, double *result) {
   if(A->columns != A->rows){
     return FAILURE;
+  }else{
+    if(A->rows == 1){
+      *result = A->matrix[0][0];
+    }else if(A->rows == 2){
+      *result = (A->matrix[0][0] * A->matrix[1][1]) - (A->matrix[0][1] * A->matrix[1][0]);
+    // - вот тут когда не 2 
+    }else if(){
+
+    }
   }
 
-  
 }
