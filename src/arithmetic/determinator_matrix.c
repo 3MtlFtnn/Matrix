10201467abb78439d223f92ba6acdd2503743d12
blisspen <blisspen@student.21-school.ru>
2024-04-09 19:16:21 +0300

    Determinant Works '

--- src/arithmetic/determinator_matrix.c
+++ src/arithmetic/determinator_matrix.c
@@ -22,40 +22,25 @@ int s21_determinant(matrix_t *A, double *result) {
   return flag;
 }
 
+
 double help_get_determinant(matrix_t *A) {
   double flag = 0.0;
+
   if (A->rows == 1) {
     flag = A->matrix[0][0];
   } else {
-    matrix_t tmp = {0};
-    s21_create_matrix(A->rows - 1, A->columns - 1, &tmp);
+    matrix_t *tmp = malloc(sizeof(matrix_t));
+    s21_create_matrix(A->rows - 1, A->columns - 1, tmp);
+
     for (int i = 0; i < A->columns; i++) {
-      s21_get_matrix(0, i, A, &tmp);
+      matrix_min(A->matrix, tmp->matrix, 0, i, A->columns);
       if (i % 2) {
-        flag -= A->matrix[0][i] * help_get_determinant(&tmp);
+        flag -= A->matrix[0][i] * help_get_determinant(tmp);
       } else {
-        flag += A->matrix[0][i] * help_get_determinant(&tmp);
+        flag += A->matrix[0][i] * help_get_determinant(tmp);
       }
     }
-    //s21_remove_matrix(&tmp);
+    s21_remove_matrix(tmp);
   }
   return flag;
-}
-void s21_get_matrix(int row, int col, matrix_t *A, matrix_t *result) {
-  int m_row = 0;
-  int m_col = 0;
-  for (int i = 0; i < A->rows; i++) {
-    if (i == row) {
-      continue;
-    }
-    m_col = 0;
-    for (int j = 0; j < A->columns; j++) {
-      if (j == col) {
-        continue;
-      }
-      result->matrix[m_row][m_col] = A->matrix[i][j];
-      m_col++;
-    }
-    m_row++;
-  }
 }
