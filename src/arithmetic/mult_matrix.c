d72c122d443a904d3d1d5d50e9d1039fced5a291
Verkartn <verkartn@student.21-school.ru>
2024-04-14 20:43:17 +0300

    иверсия готова

--- src/arithmetic/mult_matrix.c
+++ src/arithmetic/mult_matrix.c
@@ -1,1 +1,36 @@
+#include "../s21_matrix.h"
+#include <stdio.h>
 
+/* Умножает матрицу А на матрицу B и помещает результат в *result
+ *
+ *  параметры:
+ *  matrix_t *A - матрица 1
+ *  matrix_t *B - матрица 2
+ *  matrix_t *result - результат умножение
+ *
+ *  возвращает:
+ *  0 - ОК
+ *  1 - Ошибка, некорректная матрица
+ *  2 - Ошибка вычислений (несовпадающие матрицы, неверный тип матрицы)
+ *
+ */
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+  /* !(если матрица A существует и матрица B существует) */
+  if (!((A == NULL) && (B == NULL) && (result == NULL))) {
+    if (!(A->rows != B->columns)) {
+      for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < B->columns; j++) {
+          for (int k = 0; k < B->rows; k++) {
+            result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][i];
+          }
+        }
+      }
+    } else {
+      return CALC_ERR;
+    }
+  } else {
+    return FAILURE;
+  }
+
+  return SUCCESS;
+}
--- src/other_operations/matrix_invert.c
+++ src/other_operations/matrix_invert.c
@@ -1,51 +1,33 @@
 #include "../s21_matrix.h"
 
-/* Вычитает из матрицы А матрицу B и помещает результат в *result
- *
- *  параметры:
- *  matrix_t *A - матрица 1
- *  matrix_t *A - матрица 2
- *  matrix_t *result - результат вычитания
- *
- *  возвращает:
- *  0 - ОК
- *  1 - Ошибка, некорректная матрица
- *  2 - Ошибка вычислений (несовпадающие матрицы, неверный тип матрицы)
- *
- */
 int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
-  if (!(A->columns != A->rows)) {
-    matrix_t *temp = malloc(sizeof(double *));
+  if (!((A == NULL) && (result == NULL))) {
+    if (!(A->rows != A->columns)) {
+      double det = 0;
+      int status = s21_determinant(A, &det);
+      if (fabs(det) < 1e-6 || status != SUCCESS)
+        return CALC_ERR;
+      matrix_t *temp = malloc(sizeof(double **));
+      matrix_t *temp_transpose = malloc(sizeof(double **));
 
-    int result_flag = s21_create_matrix(A->rows * 2, A->columns * 2, temp);
-    if (result_flag != 0) {
-      perror("Создание матрицы не удалось");
-      return FAILURE;
-    }
+      s21_create_matrix(A->rows, A->columns, temp);
+      s21_create_matrix(A->rows, A->columns, temp_transpose);
 
-    matrix_copy(A, temp);
+      s21_calc_complements(A, temp);
+      s21_transpose(temp, temp_transpose);
 
-    for (int j = 0; j < temp->rows; j++) {
-      for (int k = A->rows; k < temp->columns; k++) {
-        if (j + A->rows == k) {
-          temp->matrix[j][k] = 1;
-        } else {
-          temp->matrix[j][k] = 0;
+      for (int x = 0; x < A->rows; x += 1) {
+        for (int y = 0; y < A->rows; y += 1) {
+          result->matrix[x][y] = temp_transpose->matrix[x][y] / det;
         }
       }
+
+      s21_remove_matrix(temp_transpose);
+      s21_remove_matrix(temp);
     }
-
-    matrix_reduce_row(temp);
-
-    for (int j = 0; j < temp->rows; j++) {
-      for (int k = A->rows; k < temp->columns; k++) {
-        A->matrix[j][k - A->rows] = temp->matrix[j][k];
-      }
-    }
-
-    matrix_copy(temp, result);
-
-    s21_remove_matrix(temp);
+  } else {
+    return CALC_ERR;
   }
-  return FAILURE;
+
+  return SUCCESS;
 }
